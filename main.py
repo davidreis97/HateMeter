1b0bf6f76250b4373d22309a91657476a030d6d8
David Reis <david.alex.reis.97@gmail.com>
2017-03-11 15:46:51 +0000

    Added a better pos-tagger and updated .gitignore

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
 import nltk
 import twitter
 from collections import Counter
+from os.path import expanduser
+from nltk.tag.stanford import StanfordPOSTagger
 
 
 def phrase_processing(person, inputs):
+
+    home = expanduser("~")
+
+    _path_to_model = home + "/PycharmProjects/Hatemeter/stanford-postagger/models/english-left3words-distsim.tagger" #You need to download and set this up before running
+    _path_to_jar = home + "/PycharmProjects/Hatemeter/stanford-postagger/stanford-postagger.jar" #You need to download and set this up before running
+
+    st = StanfordPOSTagger(model_filename=_path_to_model, path_to_jar=_path_to_jar)
 
     final_phrase = ""
     final_phrase += person + " is "
 
     adjectives = []
 
+    phrases = []
+
     for phrase in inputs:
+        phrases.insert(0, nltk.word_tokenize(phrase))
 
-        text = nltk.word_tokenize(phrase)
+    processed_text = st.tag(inputs)
 
-        processed_text = nltk.pos_tag(text)
+    print(processed_text)
 
-        for word in processed_text:
-            if word[1] == "JJ":
-                print(word)
-                adjectives.insert(0, word[0])
-
-    print(adjectives)
+    for word in processed_text:
+        if word[1] == "JJ" and len(word[0]) > 2:
+            adjectives.insert(0, word[0])
 
     count = Counter(adjectives)
 
@@ -33,17 +42,16 @@ def phrase_processing(person, inputs):
 
 if __name__ == '__main__':
 
-    person = "Obama"
+    person = "Trump"
 
-    api = twitter.Api([CREDENTIALS])
+    api = twitter.Api([CONFIDENTIAL])
 
-    twitterResponseJSON = api.GetSearch(term=person,result_type="tweet",count=1000,lang="EN")
+    twitterResponseJSON = api.GetSearch(term=person, count=100)
 
     inputs = []
 
     for tweet in twitterResponseJSON:
-        inputs.insert(0,tweet.text)
+        if tweet.text not in inputs:
+            inputs.insert(0, tweet.text)
 
-    print(phrase_processing(person,inputs))
-
-
+    print(phrase_processing(person, inputs))
